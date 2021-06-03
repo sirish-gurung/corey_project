464eb29d0d255b136013f7a73a31806ceb3efa3b
Sirish Gurung <sirishgurung@Sirishs-MacBook-Pro.local>
2021-06-03 05:18:31 -0700

    First commit

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_posts')
+
+    def __str__(self):
+        return self.title
